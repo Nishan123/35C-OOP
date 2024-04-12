67b3080a2704eb1e5ef303055eff4efd759c3ac4
Nishan123 <girinishan1234567890@gmail.com>
2024-04-12 12:34:10 +0545

    task 2 completed

--- hello.java
+++ hello.java
@@ -1,7 +1,17 @@
 public class hello{
     public static void main(String[] args){
-        /*Write a program that prints Hello World! to the console. Save the program as Hello.java */
+        /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java */
         System.out.println("Hello World");
+
+        /*2. Extend/Modify the above program to print two additional lines:
+
+ This is my first program
+
+I am on module ST4003CEM
+
+System.out.print (not println) and produces the same output. */
+System.out.print("This is my first program\nI am on moudule ST4003CEM");
+
      
     }
 }
