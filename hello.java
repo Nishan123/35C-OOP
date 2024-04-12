ab003de95bbf8148c551eac7a017583010f29263
Nishan123 <girinishan1234567890@gmail.com>
2024-04-12 12:55:02 +0545

    task 4 completed

--- hello.java
+++ hello.java
@@ -1,17 +1,31 @@
-public class hello{
-    public static void main(String[] args){
-        /*1. Write a program that prints Hello World! to the console. Save the program as Hello.java */
+public class hello {
+    public static void main(String[] args) {
+        /*
+         * 1. Write a program that prints Hello World! to the console. Save the program
+         * as Hello.java
+         */
         System.out.println("Hello World");
 
-        /*2. Extend/Modify the above program to print two additional lines:
+        /*
+         * 2. Extend/Modify the above program to print two additional lines:
+         * This is my first program
+         * I am on module ST4003CEM
+         * System.out.print (not println) and produces the same output.
+         */
+        System.out.print("This is my first program\nI am on moudule ST4003CEM");
 
- This is my first program
+        /*
+         * 3. Write a program that prints the following output:
+         * A "quoted" String is
+         * 'much' better if you learn
+         * the rules of "escape sequences."
+         * Also, "" represents an empty
+         * String. Don't forget: use \"
+         * instead of " !
+         * '' is not the same as "
+         */
 
-I am on module ST4003CEM
+         System.out.println("A \"quoted\" String is \'much\' better if you learn  the rules of \"escape sequences.\" Also, \"\" represents an empty String. Don't forget: use \" instead of \"\'\' is not the same as \"");
 
-System.out.print (not println) and produces the same output. */
-System.out.print("This is my first program\nI am on moudule ST4003CEM");
-
-     
     }
 }
