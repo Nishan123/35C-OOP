17a92fb51f1d8f94fb507f478509367a6ee19673
Nishan123 <girinishan1234567890@gmail.com>
2024-04-30 12:19:12 +0545

    for loop

---
+++ ForLoop.java
@@ -0,0 +1,36 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        // for (int i = 1; i <= 10; i++) {
+        //     for (int j = 2; j < i; j++) {
+        //         if (i % j == 0) {
+        //             break;
+        //         } else {
+        //             System.out.println(i);
+        //             break;
+        //         }
+        //     }
+        // }
+
+        
+        // for( int i=1; i<=10;i++){
+        //     System.out.println("2 *"+ i +"= "+(2*i));
+        // 
+        // }
+
+        // for(int outer = 0; outer <2 ;outer++){
+        //     for(int inner = 3; inner>=0;  inner--){
+        //         System.out.println("Intter Loop");
+        //     }
+        //     System.out.println("Outer loop");
+        // }
+
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=10; j++){
+                System.out.println(i+"x"+j+"="+i*j);
+            }
+        }
+
+
+
+    }
+}
---
+++ PrefixPostfix.java
@@ -0,0 +1,15 @@
+public class PrefixPostfix {
+    public static void main(String[] args) {
+        // Prefix operation 
+        int PrefixValue = 10;
+        System.out.println("Prefix Value in first line : "+ ++PrefixValue);
+        System.out.println("Prefix Value in second line : "+ PrefixValue);
+
+
+        // Postfix operation
+        int PostifxValue = 10;
+        System.out.println("Postfix value in first line  "+ PostifxValue++);
+        System.out.println("Postfix value in second line  "+ PostifxValue);
+
+    }
+}
